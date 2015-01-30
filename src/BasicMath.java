1f90c94ee816b703ed7fa31a38aae33904d46e07
ana <ani.dachia@gmail.com>
2015-01-30 21:12:08 +0330

    faghat mul dar in branch gharar dadam

--- src/BasicMath.java
+++ src/BasicMath.java
@@ -6,10 +6,6 @@ class BasicMath
         return a + b;
     }
 
-    public int sub(int a, int b)
-    {
-    	return a - b;
-    }
 
 	public int mul(int a, int b)
     {
--- src/Test.java
+++ src/Test.java
@@ -23,22 +23,7 @@ class Test
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
         }
     }
-    private static void testSub()
-    {
-        int a = 5;
-        int b = 6;
-        int expected = 11;
-        BasicMath bm = new BasicMath();
-        int result = bm.sub(a, b);
-        if (result == expected)
-        {
-            System.out.println("Sub: OK");
-        }
-        else
-        {
-            System.out.println("Sub: Failed, expected = " + expected + ", result = " + result);
-        }
-    }
+
         private static void testMul()
     {
         int a = 5;
