643d7eda6e46169fce696fa8ed1f5e9c3c57de6b
Nazgol Tavabi <Nazgol@Nazgols-MacBook-Air.local>
2015-01-31 00:11:53 +0330

    build

---
+++ my-app/src/main/java/com/mycompany/app/BasicMath.java
@@ -0,0 +1,23 @@
+class BasicMath
+{
+
+    public int sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int sub(int a, int b)
+
+    {
+    	return a - b;
+    }
+
+	public int mul(int a, int b)
+	{
+    	return a * b;
+    }
+    
+    public double divide (int a, int b){
+    	return a/b;
+    }
+}
